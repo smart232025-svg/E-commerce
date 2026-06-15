9ab63ecd4dd110a5df6d20cadc0a38c6258cc9ad
smart232025-svg <smart232025@gmail.com>
2026-06-15 18:11:17 +0300

    Update FloatingChatButton to open a different chat link

--- components/ui/FloatingChatButton.jsx
+++ components/ui/FloatingChatButton.jsx
@@ -5,7 +5,7 @@ export default function FloatingChatButton() {
     return (
         <button
             onClick={() =>
-                window.open("https://wa.me/201026970269", "_blank")
+                window.open("https://wa.me/201038756048", "_blank")
             }
             className="cursor-pointer fixed bottom-6 right-6 z-50 p-4 bg-linear-to-r from-green-400 to-green-600 text-white rounded-full shadow-lg hover:shadow-xl hover:scale-110 transition-all duration-300 group" aria-label="Open Chat"
         >
