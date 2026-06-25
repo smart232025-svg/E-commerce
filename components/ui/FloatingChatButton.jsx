2feea8b91d247f84eae82a057dcdf416d6405ce6
smart232025-svg <smart232025@gmail.com>
2026-06-25 17:58:07 +0300

    Reduce size of chat button icon

--- components/ui/FloatingChatButton.jsx
+++ components/ui/FloatingChatButton.jsx
@@ -9,7 +9,7 @@ export default function FloatingChatButton() {
             }
             className="cursor-pointer fixed bottom-6 right-6 z-50 p-4 bg-linear-to-r from-green-400 to-green-600 text-white rounded-full shadow-lg hover:shadow-xl hover:scale-110 transition-all duration-300 group" aria-label="Open Chat"
         >
-            <MessageCircle className="w-8 h-8 group-hover:rotate-12 transition-transform duration-300" />
+            <MessageCircle className="w-4 h-4 group-hover:rotate-12 transition-transform duration-300" />
         </button >
     )
 }
